8a4a1540eac9eb3b15f4f81ea537d8358db3e74b
Aurélien Aptel <aurelien.aptel@gmail.com>
2014-10-29 15:26:42 +0100

    canonical bandcamp url

--- bandcamp.py
+++ bandcamp.py
@@ -6,14 +6,27 @@ sys.path.insert(0, 'lib')
 import requests as R
 from bs4 import BeautifulSoup as BS
 import re
+import cgi
 
 URL_REGEX = re.compile(r'(?:https?://)?[0-9a-z-]+\.bandcamp\.com(?:/(?:album|track)/[0-9a-z-]+)?(?:#[0-9a-z-]*)?')
+URL_CAPTURE_REGEX = re.compile(r'(?:https?://)?([0-9a-z-]+)\.bandcamp\.com(/(?:album|track)/[0-9a-z-]+)?(?:#[0-9a-z-]*)?')
+
 
 def valid_url(url):
     return re.match(URL_REGEX, url)
 
 def extract_urls(text):
     return re.findall(URL_REGEX, text)
+
+def repl_url(m):
+    t = m.group(0)
+    if str.startwith('http', t):
+        return '<a href="%s">%s</a>' % (t,t)
+    else:
+        return '<a href="https://%s">%s</a>' % (t,t)
+
+def urlify(text):
+    return re.sub(URL_REGEX, repl_url, text)
 
 class BandcampUrl:
     def __init__(self, url):
@@ -34,6 +47,13 @@ class BandcampUrl:
             self._artist = self.soup().select('p#band-name-location span.title')[0].get_text()
         return self._artist
 
+    def canonical(self):
+        m = re.match(URL_CAPTURE_REGEX, self._url)
+        base = 'https://%s.bandcamp.com' % m.group(1)
+        if m.group(2):
+            base += m.group(2)
+        return base
+
 #
 # TESTS
 #
@@ -48,7 +68,6 @@ class BandcampTests(unittest.TestCase):
         self.assertTrue(valid_url('https://twistpillar.bandcamp.com/album/unity-plaza-deluxe'))
         self.assertTrue(valid_url('https://twistpillar.bandcamp.com/track/b-ss'))
         self.assertTrue(valid_url('gxrxnimx.bandcamp.com'))
-
 
         self.failIf(valid_url('https://bandcamp.com'))
 
@@ -70,7 +89,31 @@ Someone described it as "nightmare soundtracks", I like that description.
 
         self.assertEqual(extract_urls(t), urls)
 
+    def test_canonical(self):
+        #
+        # EQUAL TESTS
+        #
 
+        c = 'https://test.bandcamp.com'
+        urls = ['test.bandcamp.com', 'http://test.bandcamp.com', 'http://test.bandcamp.com/', 'test.bandcamp.com/#zob']
+        self.assertEqual(BandcampUrl(c).canonical(), c)
+        for u in urls:
+            self.assertEqual(BandcampUrl(u).canonical(), c)
+
+        c = 'https://test.bandcamp.com/album/zob'
+        urls = ['test.bandcamp.com/album/zob#foo', 'http://test.bandcamp.com/album/zob']
+        self.assertEqual(BandcampUrl(c).canonical(), c)
+        for u in urls:
+            self.assertEqual(BandcampUrl(u).canonical(), c)
+
+        #
+        # NOT EQUAL
+        #
+
+        c = 'https://test.bandcamp.com/album/zob'
+        urls = ['test.bandcamp.com', 'http://test.bandcamp.com/track/zob']
+        for u in urls:
+            self.assertNotEqual(BandcampUrl(u).canonical(), c)
 
 if __name__ == '__main__':
     unittest.main()
