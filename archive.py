500af4f5c09e4b16223540ae237cf8f7c9a4f97c
Aurélien Aptel <aurelien.aptel@gmail.com>
2014-11-02 19:20:10 +0100

    make stand-alone archive script with proper logging (file, email)

--- archive.py
+++ archive.py
@@ -1,81 +1,188 @@
-#!/bin/env python2
+#!/usr/bin/env python
 
-import board
 import cPickle as pickle
 import argparse
 import logging
 import logging.handlers
 import sys
+import errno
+import time
+import os
+import json
+import gzip
+import requests
 
-log = logging.getLogger('archive')
-log.setLevel(logging.WARNING)
-log.addHandler(logging.StreamHandler(sys.stderr))
+LOG = logging.getLogger('archive')
 
-DEFAULT_DB = 'mu_band_db.pickle'
+DEFAULT_DB  = 'mu_db.pkl.gz'
+CATALOG_URL = 'http://a.4cdn.org/mu/catalog.json'
+THREAD_URL  = 'http://a.4cdn.org/mu/thread/%d.json'
+DOWNLOAD_DELAY = .6
+LOG_DIR = 'logs'
+NB_DL = 0
 
+def init_log(level=logging.INFO, email=False, file=False):
+    LOG.setLevel(level)
 
-def index(e, list):
+    fmt = logging.Formatter("%(asctime)s %(levelname)-5s %(message)s", datefmt="%Y-%m-%d %H:%M")
+    console = logging.StreamHandler(sys.stderr)
+    console.setFormatter(fmt)
+    LOG.addHandler(console)
+
     try:
-        return list.index(e)
-    except ValueError:
-        return -1
+        os.makedirs(LOG_DIR)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            LOG.error("can't create log dir '%s': %s", LOG_DIR, e.strerror)
+
+
+    if email and os.path.isfile("/usr/sbin/sendmail"):
+        h = MailHandler('mu-archive@diobla.info', ['aurelien.aptel@gmail.com'], 'mu-archive log report', 100)
+        h.setLevel(logging.WARNING)
+        h.setFormatter(fmt)
+        LOG.addHandler(h)
+    if file:
+        h = logging.FileHandler(os.path.join(LOG_DIR, "log"))
+        h.setLevel(logging.INFO)
+        h.setFormatter(fmt)
+        LOG.addHandler(h)
+
+# logging handler that sends emails using sendmail
+class MailHandler(logging.handlers.BufferingHandler):
+    def __init__(self, fromaddr, toaddrs, subject, capacity=10):
+        logging.handlers.BufferingHandler.__init__(self, capacity)
+        self.fromaddr = fromaddr
+        self.toaddrs = toaddrs
+        self.subject = subject
+
+    def flush(self):
+        if len(self.buffer) > 0:
+            p = os.popen("/usr/sbin/sendmail -t", "w")
+            msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" % (self.fromaddr, string.join(self.toaddrs, ","), self.subject)
+            for record in self.buffer:
+                s = self.format(record)
+                #print s
+                msg += s + "\r\n"
+            p.write(msg)
+            r = p.close()
+            self.buffer = []
+
 
 def load(db):
+    LOG.debug('loading db %s', db)
+
+    start = time.time()
     try:
-        f = open(db)
-        return pickle.load(f)
+        f = gzip.open(db)
+        r = pickle.load(f)
     except IOError:
-        return []
+        r = {}
 
-def save(thrs, db):
-    f = open(db, 'w+')
-    pickle.dump(thrs, f)
-
-def open_threads(thrs):
-    r = []
-    for t in thrs:
-        if t.is_open():
-            r.append(t)
+    LOG.info('db loaded in %.03fs', time.time()-start)
     return r
 
-def update_threads(db):
+def save(thrs, db):
+    LOG.debug('saving db %s', db)
+
+    start = time.time()
+    f = gzip.open(db, 'w+')
+    pickle.dump(thrs, f)
+    LOG.info('db saved in %.03fs', time.time()-start)
+
+def get(url):
+    global NB_DL
+    NB_DL += 1
+    LOG.debug('fetch url %s', url)
+
+    r = requests.get(url)
+
+    time.sleep(DOWNLOAD_DELAY)
+    return r
+
+def get_file_name():
+    n = int((time.time() + 0.5) * 1000)
+    while os.path.exists(os.path.join(LOG_DIR, str(n))):
+        n += 1
+    return os.path.join(LOG_DIR, str(n))
+
+def dump_object(o):
+    fn = get_file_name()
+    f = open(fn, 'w+')
+    pickle.dump(o, f)
+    return fn
+
+def get_thread(no):
+    LOG.debug('fetch thread %d', no)
+
+    r = get(THREAD_URL % no)
+    if r.status_code == 404:
+        LOG.info('thread %d is dead (404)', no)
+        return None
+    if r.status_code == 200:
+        return r
+    else:
+        LOG.error('fetch thread %d: status_code = %d, response dump in %s', no, r.status_code, dump_object(r.text))
+        return None
+
+def update(db):
+    start = time.time()
     thrs = load(db)
 
-    openthrs = open_threads(thrs)
-    openthr_ids = [thr.id() for thr in openthrs]
+    r = get(CATALOG_URL)
+    if r.status_code != 200:
+        LOG.error('fetch catalog: status_code = %d, response dump in %s', r.status_code, dump_object(r.text))
+        raise Exception("fetch catalog failed")
 
-    catathrs = board.get_catalog_threads()
-    catathr_ids = [thr.id() for thr in catathrs]
+    cata = r.json()
+    nb_thr = 0
 
-    # 1. update or add threads from the catalog
-    for thr in catathrs:
-        log.info("T %d (catalog)" % thr.id())
-        i = index(thr.id(), openthr_ids)
-        if i >= 0:
-            if thr.newer_than(openthrs[i]):
-                # fetch full thread and update entry in db
-                log.info("fetch & update")
-                openthrs[i].update()
-        else:
-            # fetch full thread and create entry in db
-            log.info("fetch & create")
-            thr.update() # fetch full thread
-            thrs.append(thr)
-
-    # 2. close any open thread not in the catalog anymore
-    for thr in openthrs:
-        thr.update()
+    # start by the last threads to limit 404s
+    for page in reversed(cata):
+        for t in reversed(page['threads']):
+            n = t['no']
+            r = get_thread(n)
+            if r is not None:
+                thrs[n] = r.json()
+                nb_thr += 1
 
     save(thrs, db)
+    LOG.info('update done in %.03fs (%d fetch, %d threads)', time.time()-start, NB_DL, nb_thr)
+
+def dump_json(db, thr=None, pretty=None):
+    thrs = load(db)
+    opts = {}
+
+    if pretty:
+        opts['sort_keys'] = True
+        opts['indent'] = 4
+        opts['separators'] = (',', ': ')
+
+    if thr:
+        json.dump(thrs.get(thr, {}), sys.stdout, **opts)
+    else:
+        json.dump(thrs, sys.stdout, **opts)
 
 def main():
-    parser = argparse.ArgumentParser(description='Archive /mu/ bandcamp threads')
+    parser = argparse.ArgumentParser(description='Archive /mu/ threads')
     parser.add_argument('-f', '--file', help='use file as pickle db', default=DEFAULT_DB)
-    parser.add_argument('-v', '--verbose', help='verbose', action='store_true')
+    parser.add_argument('-j', '--json', help='dump on stdout as json', action='store_true')
+    parser.add_argument('-p', '--pretty', help='pretty print json output', action='store_true')
+    parser.add_argument('-t', '--thread', help='dump single thread', type=int)
+    parser.add_argument('-v', '--verbose', help='verbose, show debug messages', action='store_true')
+    parser.add_argument('-l', '--log', help='log output type', action='append', default=[], choices=['file', 'email'])
     args = parser.parse_args()
-    if args.verbose:
-        log.setLevel(logging.INFO)
-    update_threads(args.file)
+
+    init_log(level=(logging.DEBUG if args.verbose else logging.INFO),
+             file=('file' in args.log),
+             email=('email' in args.log))
+
+    if args.json:
+        dump_json(args.file, thr=args.thread, pretty=args.pretty)
+    else:
+        update(args.file)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except:
+        LOG.exception('archive exception')
